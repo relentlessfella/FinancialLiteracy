16f3bd4772fa50433155e23220de4f9634237f5d
Nurgali <nrknrps@gmail.com>
2024-03-19 17:14:49 +0500

    updated: header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,10 +6,11 @@ import logo from '../../public/assets/FinLit.svg';
 import magnifier from '../../public/assets/magnifier.svg';
 import dropdown from '../../public/assets/dropdown.svg';
 import web from '../../public/assets/web.svg';
-import './Header.css';
+import styles from './component.module.css';
 import Link from 'next/link';
 import divider from '../../public/assets/Divider.svg';
 import { useRouter } from 'next/navigation';
+import { poppins } from '@/app/login/page';
 
 const Header = () => {
   const router = useRouter();
@@ -19,10 +20,10 @@ const Header = () => {
       style={{
         display: 'flex',
         justifyContent: 'space-between',
-        paddingLeft: '40px',
-        paddingRight: '40px',
+        // paddingLeft: '40px',
+        // paddingRight: '40px',
       }}
-      className="header">
+      className={`${styles.header} ${poppins.variable}`}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <Image
           src={logo}
@@ -33,42 +34,32 @@ const Header = () => {
           style={{
             borderRadius: '10px',
             border: '1px solid gray',
-            width: '444px',
-            height: '62px',
+            // width: '444px',
+            // height: '62px',
             display: 'flex',
             justifyContent: 'space-between',
           }}>
           {/* <div style={{ marginTop: 'auto', marginBottom: 'auto' }}>Search</div> */}
           <div style={{ display: 'flex', alignItems: 'center' }}>
-            <input
-              type="text"
-              placeholder="Search..."
-              style={{
-                padding: '20px',
-                width: '350px',
-                borderRadius: '13px',
-                border: 'none',
-                outline: 'none',
-              }}
-            />
+            <input type="text" placeholder="Search..." className={styles.input} />
             <Image src={magnifier} width={20} height={20} style={{ margin: '15px' }} />
           </div>
         </div>
       </div>
       <ul style={{ display: 'flex', listStyle: 'none' }}>
-        <li className="header_li">
+        <li className={styles.header_li}>
           <Link href={'/'} style={{ textDecoration: 'none', color: 'black' }}>
             <div>Courses</div>
             {/* <Image src={divider } width={51} height={19} /> */}
           </Link>
         </li>
-        <li className="header_li">
+        <li className={styles.header_li}>
           <Link href={'/'} style={{ textDecoration: 'none', color: 'black' }}>
             <div>Financial Simulator</div>
             {/* <Image src={divider} width={51} height={19} /> */}
           </Link>
         </li>
-        <li style={{ display: 'flex' }} className="header_li">
+        <li style={{ display: 'flex' }} className={styles.header_li}>
           <div>
             <Link href={'/profile'} style={{ textDecoration: 'none', color: 'black' }}>
               <div>Nussupekov Arnibek</div>
@@ -77,7 +68,7 @@ const Header = () => {
           </div>
           <Image style={{ paddingTop: '0px' }} src={dropdown} width={10} height={10} />
         </li>
-        <li className="header_li">
+        <li className={styles.header_li}>
           <Image src={web} width={30} height={30} />
         </li>
       </ul>
